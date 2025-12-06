94d3f74577e2a28d2348ab4c23155f1f1ad13edb
Kamil Śliwa <137560154+Kamivarr@users.noreply.github.com>
2025-12-06 19:51:38 +0100

    add .gitignore file and update assembly informational version

--- backend/obj/Debug/net8.0/SplitMate.Api.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/SplitMate.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SplitMate.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6a11ecaa5b31a2dfff4a8b713f19192363be55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb448980302d2e31d91ebcb58c498adfc745f0f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SplitMate.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SplitMate.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
